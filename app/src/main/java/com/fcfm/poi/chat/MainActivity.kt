9838c6bc1f883939f47676c5044e265ffe0f1ceb
JesusMtzz <52387097+JesusMtzz@users.noreply.github.com>
2021-04-29 00:13:56 -0500

    a mimir

--- app/src/main/java/com/fcfm/poi/chat/MainActivity.kt
+++ app/src/main/java/com/fcfm/poi/chat/MainActivity.kt
@@ -4,36 +4,84 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.fcfm.poi.chat.modelos.Usuario
 import kotlinx.android.synthetic.main.activity_main.*
+
+import com.google.firebase.database.*
 
 
 class MainActivity : AppCompatActivity() {
 
+
+    private val database = FirebaseDatabase.getInstance()
+    private val chatRef = database.getReference("usuario")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//Chat Personal
+
+
+
+
+
 
         btnChatPersonal.setOnClickListener {
 
             val nombreUsuario = txtUsuario.text.toString()
+            //codigo nuevo
+            var entrar = false;
 
-            if (nombreUsuario.isEmpty()) {
 
-                Toast.makeText(this, "Falta usuario", Toast.LENGTH_SHORT).show()
-            } else {
+            chatRef.addValueEventListener(object : ValueEventListener {
 
-                val intentChat = Intent(this, ChatActivity::class.java)
-                intentChat.putExtra("nombreUsuario", nombreUsuario)
+                override fun onDataChange(snapshot: DataSnapshot) {
 
-                startActivity(intentChat)
-            }
+
+                    for (snap in snapshot.children) {
+
+                        val usuario: Usuario = snap.getValue(Usuario::class.java) as Usuario
+                        if (usuario.nombre == nombreUsuario)
+                            entrar = true;
+
+
+                        if (nombreUsuario.isEmpty()) {
+
+                            Toast.makeText(this@MainActivity, "Falta usuario", Toast.LENGTH_SHORT).show()
+                        } else {
+                            if(entrar) {
+                                val intentChat = Intent(this@MainActivity, ChatActivity::class.java)
+                                intentChat.putExtra("nombreUsuario", nombreUsuario)
+
+                                startActivity(intentChat)
+                                entrar = false;
+                            } else {
+
+                                Toast.makeText(this@MainActivity, "No es tu chat", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+
+
+                    }
+
+
+                }
+                override fun onCancelled(error: DatabaseError) {
+
+                    Toast.makeText(this@MainActivity, "Error", Toast.LENGTH_SHORT).show()
+                }
+            })
+
+
+
+
         }
 
-
+//Chat Grupal
         btnChatGrupal.setOnClickListener {
 
-            val nombreUsuario = txtUsuario.text.toString()
 
+            val nombreUsuario = txtUsuario.text.toString()
             if (nombreUsuario.isEmpty()) {
 
                 Toast.makeText(this, "Falta usuario", Toast.LENGTH_SHORT).show()
